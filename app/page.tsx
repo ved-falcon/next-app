e7e4505cfde8af26d5d5bc192d75fe71f08d5294
ved-falcon <ved@falconsystem.com>
2024-10-17 13:14:09 -0500

    remove issue spots

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-
-
-const prisma = new PrismaClient();
-
-async function getStudents(){
-    const students = await prisma.student.findMany()
-    return students
-}
-
-
-export default async function Students(){
-    const students = await getStudents()
-    return (
-      <div>
-        <h1>Students</h1>
-        <ul>
-          {students.map(student => {
-            return <li key={student.id}>{student.name}</li>
-          })}
-        </ul>
-        </div>
-    )
+export default function Home() {
+  return <div>
+    <h1>Hello World</h1>
+    <p>This is a web app deployed on Vercel.</p>
+  </div>
 }
